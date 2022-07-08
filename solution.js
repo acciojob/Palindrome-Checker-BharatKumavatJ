956c4f9cb469c8ca9da2bba54f8c64be59cddc55
BharatKumavatJ <98304786+BharatKumavatJ@users.noreply.github.com>
2022-07-08 19:46:17 +0530

    palindrome done

--- solution.js
+++ solution.js
@@ -2,5 +2,14 @@
 
 function palindrome(str){
 
+    var e = str.length - 1;
+    for(var s in str){
+        if(s >= e){
+            return true;
+        }
+        if(str[s] != str[e]) return false;
+        e--;
+    }
+    return true;
 }
 module.exports = palindrome
